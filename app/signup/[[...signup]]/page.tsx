2a4ad40e7b2201dfe1c54095a2902d0ebc3380fa
Harsh <harshpatil10c16@gmail.com>
2024-09-30 21:46:07 +0530

    added 1

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -5,7 +5,7 @@ export default function SignUpPage() {
 	return (
 		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
 			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-				<SignUp />
+				<SignUp redirectUrl="https://agentcoach-ai-new.vercel.app/ai"/>
 			</div>
 		</div>
 	);
