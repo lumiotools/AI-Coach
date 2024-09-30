fc8b418cf6b427c67c0e1e30fbfaa964deba67fd
Harsh <harshpatil10c16@gmail.com>
2024-09-30 21:57:27 +0530

    added 3

--- app/signin/[[...signin]]/page.tsx
+++ app/signin/[[...signin]]/page.tsx
@@ -4,8 +4,8 @@ export default function SignInPage() {
 	return (
 		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
 			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-				{/* <SignIn redirectUrl="https://agentcoach-ai-new.vercel.app/ai" forceRedirectUrl="https://agentcoach-ai-new.vercel.app/ai" /> */}
-				<SignIn signUpUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signin`}/>
+				<SignIn redirectUrl="https://agentcoach-ai-new.vercel.app/ai" forceRedirectUrl="https://agentcoach-ai-new.vercel.app/ai" afterSignInUrl='https://agentcoach-ai-new.vercel.app/ai'/>
+				{/* <SignIn signUpUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signin`}/> */}
 			</div>
 		</div>
 	);
--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -5,8 +5,8 @@ export default function SignUpPage() {
 	return (
 		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
 			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-				{/* <SignUp redirectUrl="https://agentcoach-ai-new.vercel.app/ai" forceRedirectUrl="https://agentcoach-ai-new.vercel.app/ai"/> */}
-				<SignUp signInUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signup`}/>
+				<SignUp redirectUrl="https://agentcoach-ai-new.vercel.app/ai" forceRedirectUrl="https://agentcoach-ai-new.vercel.app/ai" afterSignUpUrl="https://agentcoach-ai-new.vercel.app/ai"/>
+				{/* <SignUp signInUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signup`}/> */}
 			</div>
 		</div>
 	);
