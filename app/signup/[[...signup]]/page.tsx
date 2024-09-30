2ba8ee3134e378af7d36576842fec46a815587d4
Harsh <harshpatil10c16@gmail.com>
2024-09-30 21:51:47 +0530

    added 2

--- app/signup/[[...signup]]/page.tsx
+++ app/signup/[[...signup]]/page.tsx
@@ -1,11 +1,12 @@
-import { SignUp } from '@clerk/nextjs';
+import { SignIn, SignUp } from '@clerk/nextjs';
 
 
 export default function SignUpPage() {
 	return (
 		<div className="relative h-screen w-full overflow-hidden bg-gray-900">
 			<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-				<SignUp redirectUrl="https://agentcoach-ai-new.vercel.app/ai"/>
+				{/* <SignUp redirectUrl="https://agentcoach-ai-new.vercel.app/ai" forceRedirectUrl="https://agentcoach-ai-new.vercel.app/ai"/> */}
+				<SignUp signInUrl={`${process.env.NEXT_PUBLIC_APP_URL}/signup`}/>
 			</div>
 		</div>
 	);
