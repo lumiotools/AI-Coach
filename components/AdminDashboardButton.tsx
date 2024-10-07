a86982e4fa81ac39f9f8053fd9c84c64da544e52
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-07 19:05:41 +0530

    refactor admin btn

--- components/AdminDashboardButton.tsx
+++ components/AdminDashboardButton.tsx
@@ -7,12 +7,12 @@ const AdminDashboardButton = ({
 }) => {
   if (userEmail !== undefined && userEmail === "salunkhekunal594@gmail.com") {
     return (
-      <div className="text-center p-4">
+      <div className="text-center px-8">
         <a
           href="https://admin-dashboard-jade-gamma.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
-          className="bg-[#2F76FF] rounded-full px-4 py-2 flex self-stretch items-center justify-center cursor-pointer"
+          className="bg-[#2F76FF] text-sm rounded-full px-4 py-2 flex self-stretch items-center justify-center cursor-pointer"
         >
           Admin Dashboard
         </a>
