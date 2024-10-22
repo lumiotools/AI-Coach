a365505fd83a2226b3d547974d02bf0868f756f3
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-22 16:27:34 +0530

    send email

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
 import { clerkClient } from "@clerk/nextjs/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
   try {
@@ -67,6 +70,31 @@ export async function POST(request: Request) {
         },
       },
     });
+
+    console.log(paymentInfo.planDetails.name);
+
+    if (paymentInfo.planDetails.name === "Team") {
+      const emailRes = await resend.emails.send({
+        from: "aiagentcoach@teamlumio.ai",
+        to: [paymentInfo.userEmail],
+        subject: "Request for Team Member Emails",
+        html: `
+        <p>Hi there!</p>
+        <p>Thank you for choosing our <strong>Comprehensive AI Coaching Solutions for Real Estate Teams</strong>.</p>
+        <p>To get started with your plan, please send us the email addresses of the team members you'd like to add. As part of the plan, you can add up to 5 users to your account. Once we have their details, we'll ensure their access is set up immediately.</p>
+        <p>If you have any questions, feel free to reach out.</p>
+        `,
+      });
+
+      console.log("Email response:", emailRes);
+
+      if (emailRes.error) {
+        return NextResponse.json(
+          { error: "Failed to send welcome email" },
+          { status: 500 }
+        );
+      }
+    }
 
     return NextResponse.json(
       { message: "User payment info updated successfully" },
